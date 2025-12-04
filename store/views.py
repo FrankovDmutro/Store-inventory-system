c3f9ea3fc800ba70f2b2e061c7073df35c75cc0f
FrankovDmutro <dmytro.frankov@lnu.edu.ua>
2025-12-04 18:28:01 +0200

    Implement product listing feature with view, template, and URL configuration

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    # 1. Дістаємо всі товари з бази даних
+    products = Product.objects.all()
+    
+    # 2. Віддаємо їх у файл дизайну (шаблон)
+    return render(request, 'store/product_list.html', {'products': products})
